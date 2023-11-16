3cbde2d789523ce25f6419661b4aba09657560a9
melf <mel.f@bestgame-co.com>
2023-11-16 14:40:42 +0800

    --

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-document.getElementById('button').addEventListener('click', function() {
-  var files = document.getElementById('file').files;
-  if (files.length > 0) {
-    getBase64(files[0]);
-  }
-});
+window.onload = function() {
 
-function getBase64(file) {
-   var reader = new FileReader();
-   reader.readAsDataURL(file);
-   reader.onload = function () {
-     console.log(reader.result);
-   };
-   reader.onerror = function (error) {
-     console.log('Error: ', error);
-   };
+	document.getElementById('button').addEventListener('click', function() {
+		var files = document.getElementById('file').files;
+		if (files.length > 0) {
+			getBase64(files[0]);
+		}
+	});
+
+	function getBase64(file) {
+		var reader = new FileReader();
+		reader.readAsDataURL(file);
+		reader.onload = function() {
+			console.log(reader.result);
+		};
+		reader.onerror = function(error) {
+			console.log('Error: ', error);
+		};
+	}
 }
